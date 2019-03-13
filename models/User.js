64e5b2802b0521cf17f52cf71b8be1acd50bb041
Javier <javiermaker@gmail.com>
2019-03-14 00:15:12 +0100

    card from foursquare

--- models/User.js
+++ models/User.js
@@ -9,7 +9,8 @@ const userSchema = new Schema({
     password: String,
     name: { type: String, default: "Popino, the Dogtor" },
     bio: { type: String, default: "Ayer se fue tomo sus cosas y se puso a navegar una camisa un pantalon vaquero y una cancion donde ira, donde ira y decidio batirse en duelo con el mar y recorer el mundo en su velero y navegar laylala navegar. Y se marcho y a su barco le llamo libertad y en le cielo descubrio gavioootas y pinto estelas en el mar" },
-    photo: { type: String, default: "/images/users/popino.jpg" }
+    photo: { type: String, default: "/images/users/popino.jpg" },
+    destiniesId: [{ type: Schema.Types.ObjectId, ref: 'Destiny' }]
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
