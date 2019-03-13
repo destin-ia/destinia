a782c87fd4e209aed204c5b83bf2a69fe9e52169
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2019-03-13 15:46:15 +0100

    add leaflet

--- models/Venue.js
+++ models/Venue.js
@@ -7,22 +7,29 @@ const venueSchema = new Schema({
     id: String,
     name: String,
     contact: {
-        twitter: String,
         phone: String,
-        formattedPhone: String
+        formattedPhone: String,
+        twitter: String,
+        instagram: String,
+        facebook: String,
+        facebookUsername: String,
+        facebookName: String
     },
     location: {
         address: String,
         crossStreet: String,
-        city: String,
-        state: String,
-        postalCode: String,
-        country: String,
         lat: Number,
         lng: Number,
+        postalCode: String,
+        cc: String,
+        city: String,
+        state: String,
+        country: String,
+
         distance: Number // in meters
     },
-    categories: [String],
+    categories: [],
+    verified: { type: Boolean },
     stats: {
         checkinsCount: Number,
         usersCount: Number,
@@ -30,7 +37,31 @@ const venueSchema = new Schema({
     },
     url: String,
     hours: String,
-    popular: String
+    popular: String,
+    menu: String,
+    price: {
+        tier: Number,
+        message: String
+    },
+    rating: Number,
+    hereNow: String,
+    storeId: String,
+    description: String,
+    createdAt: String,
+    mayor,
+    tips,
+    listed,
+    beenHere,
+    shortUrl,
+    canonicalUrl,
+    photos,
+    likes,
+    like,
+    dislike,
+    phrases,
+    attributes,
+    roles,
+    page
 }, { timestamps: true });
 
 const Venue = mongoose.model('Venue', venueSchema);
--- routes/user.js
+++ routes/user.js
@@ -11,27 +11,14 @@ const bcryptSalt = 10;
 
 Router.get("/dashboard", (req, res, next) => res.render("user/dashboard", { errorMessage: `` }));
 
-Router.post("/dashboard/geocode", (req, res, next) => {
-    axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${req.body.geocode}.json?access_token=pk.eyJ1IjoibWFrZXJkZXYiLCJhIjoiY2p0M2E3czQ1MW8zNzQ0cGF3aWhqbmM0NSJ9.LsxCUIsThXEOpzlW233seg`)
-        .then(response => {
-            const postGeo = {
-                posGeocodeLat: response.data.features[0].center[0],
-                posGeocodeLon: response.data.features[0].center[1]
-            };
-            res.json(postGeo);
-        })
-        .catch(err => next(err));
-    axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${process.env.CLIENTID}&client_secret=${process.env.SECRET}&v=20180323&limit=50&near=${req.body.geocode}`)
+Router.post("/dashboard", (req, res, next) => {
+    axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${process.env.CLIENTID}&client_secret=${process.env.SECRET}&v=20190202&near=${req.body.place}&radius=${req.body.RADIUS}`) //&v=20120609
+        // axios.get(`https://api.foursquare.com/v2/venues/suggestcompletion?client_id=${process.env.CLIENTID}&client_secret=${process.env.SECRET}&v=20180323&limit=50&near=${req.body.place}&radius=${req.body.RADIUS}&query=historic`)
         .then(response => {
             console.log(`Response from the API is: `, response.data.response.venues);
+            res.json(response.data.response.venues)
         })
         .catch(err => next(err));
-});
-
-Router.post("/dashboard/routeinit", (req, res, next) => {
-    const { lat, lon } = req.body;
-    console.log(req.body);
-    // res.json()
 });
 
 module.exports = Router;
